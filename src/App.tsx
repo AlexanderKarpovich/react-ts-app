d7eaf60f6fecf4be7ae669b38ba7ffdf006ff12c
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-09 16:22:55 +0300

    Card component added. Props implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Card, { CardVariant } from './components/Card';
 
 const App = () => {
   return (
     <div>
-      Работает!
+      <Card width='200px' height='200px' variant={CardVariant.outlined} 
+        onClick={(number, action) => 
+          { 
+            alert('click' + number); 
+            action(++number);
+          }}>
+        <button>Кнопка</button>
+      </Card>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
 
-const Card = () => {
+export enum CardVariant {
+  outlined = 'outlined',
+  primary = 'primary'
+}
+
+interface CardProps {
+  width?: string;
+  height?: string;
+  children?: React.ReactNode;
+  variant: CardVariant;
+  onClick: (num: number, action: React.Dispatch<React.SetStateAction<number>>) => void;
+}
+
+const Card: FC<CardProps> = 
+({
+  width, 
+  height, 
+  variant,
+  onClick,
+  children
+}) => {
+  const [state, setState] = useState(0);
   return (
-    <div>Card</div>
+    <div style={{width, height, 
+      border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+      background: variant === CardVariant.primary ? 'lightgray' : ''}}
+      onClick={() => onClick(state, setState)}>
+      {children}
+    </div>
   )
 }
 
